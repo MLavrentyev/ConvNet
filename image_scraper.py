4ee03ffad4c58ceb183b94c173884caabbc0433b
Mark Lavrentyev <lavrema@outlook.com>
2017-05-15 15:55:33 -0400

    Get cx code for Custom Search Engine

--- image_scraper.py
+++ image_scraper.py
@@ -2,16 +2,27 @@ import requests
 
 class ImageScraper(object):
 
-    def __init__(self):
+    def __init__(self, apiKey):
         
         self.baseUrl = "https://www.googleapis.com/customsearch/v1"
-        self.apiKey = "AIzaSyBjRRMtqV4VdybDPjr-tNObKI6qbAukdYE"
+        self.apiKey = apiKey
 
         self.params = {"searchType": "image",
                        "imgColorType": "color",
                        "filter": "0",
-                       "safe": "medium"}
+                       "safe": "medium",
+                       "key": apiKey}
 
     def sendRequest(self, query):
         # query - string the query to be sent in the Custom Search
-        self.params["q"]: query
+        self.params["q"] = query
+        self.params["cx"] = "002614461317739606024:pdrv0eu2ihq"
+        response = requests.get(self.baseUrl, params=self.params)
+
+        print(response)
+
+        
+
+imS = ImageScraper("AIzaSyBjRRMtqV4VdybDPjr-tNObKI6qbAukdYE")
+values = imS.sendRequest("cat")
+
