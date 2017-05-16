0eb3829c0453256e341f81f3caccd32916cf8dbf
Mark Lavrentyev <lavrema@outlook.com>
2017-05-16 13:00:03 -0400

    Fix image importing to return 2d array instead of 1d

--- img_preparer.py
+++ img_preparer.py
@@ -15,11 +15,13 @@ class ImageConverter(object):
         img = Image.open(imgPath)
 
         if img.size != (self.imgWidth, self.imgHeight):
-            img.resize((self.imgWidth, self.imgHeight), Image.LANCZOS)
+            img = img.resize((self.imgWidth, self.imgHeight), Image.LANCZOS)
 
-                
-        rPix = np.array(list(img.getdata(band=0)))
-        gPix = np.array(list(img.getdata(band=1)))
-        bPix = np.array(list(img.getdata(band=2)))
+        rPix = np.array(list(img.getdata(band=0))).reshape(self.imgWidth,
+                                                           self.imgHeight)
+        gPix = np.array(list(img.getdata(band=1))).reshape(self.imgWidth,
+                                                           self.imgHeight)
+        bPix = np.array(list(img.getdata(band=2))).reshape(self.imgWidth,
+                                                           self.imgHeight)
 
         return [rPix, gPix, bPix]
