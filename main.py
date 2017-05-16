330b48d2726403c3fb3a1b27bf495e69929902f2
Mark Lavrentyev <lavrema@outlook.com>
2017-05-16 13:34:20 -0400

    Add method to retrieve training data in proper format

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from image_scraper import ImageScraper
+from img_preparer import ImageConverter
+import os
 
 
 def getTrainData(words, numImages):
@@ -9,4 +11,24 @@ def getTrainData(words, numImages):
 
     for word in words:
         imS.downloadImages(word, numImages)
+
+
+#getTrainData(["cat", "giraffe", "house", "tree"], 100)
+
+def importTrainData():
+    imC = ImageConverter((150,100))
+    
+    allImgs = []
+    allLabels = []
+    #labels and images will be aligned
+    
+    for folder in os.listdir("trainingData/"):
+        for image in os.listdir("trainingData/" + folder):
+            allImgs.append(imC.imageToNPArray("trainingData/" +
+                                              folder + "/" + image))
+            allLabels.append(folder)
+
+    return allImgs, allLabels
+
+trainData, trainLabels = importTrainData()
 
