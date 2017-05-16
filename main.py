b62ec6a88d6394f4a73f3eaa5cf0eeca483f5dd7
Mark Lavrentyev <lavrema@outlook.com>
2017-05-16 09:22:18 -0400

    Add main file to run the program

---
+++ main.py
@@ -0,0 +1,9 @@
+from image_scraper import ImageScraper
+
+
+def getTrainData(words, numImages):
+    # words - list of search terms to use
+    # numImages - number of images to get for each category
+
+    pass
+
