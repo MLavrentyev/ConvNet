e4463b1dc3c04c037bf4a692e8764e40fc4718a6
Mark Lavrentyev <lavrema@outlook.com>
2017-05-16 11:17:01 -0400

    Create class to prepare images for the CNN

---
+++ img_preparer.py
@@ -0,0 +1,11 @@
+import PIL
+import numpy as np
+
+class ImageConverter(object):
+
+    def __init__(self, imageSize):
+        # imageSize - tuple (w,h) to normalize images to
+
+        self.imgWidth = imageSize[0]
+        self.imgHeight = imageSize[1]
+        
