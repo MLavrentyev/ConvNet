a66fa6e729aad67ea841a19bb35432e0fa914abc
Mark Lavrentyev <lavrema@outlook.com>
2017-05-16 11:19:33 -0400

    Specify image preparing methods

--- img_preparer.py
+++ img_preparer.py
@@ -9,3 +9,9 @@ class ImageConverter(object):
         self.imgWidth = imageSize[0]
         self.imgHeight = imageSize[1]
         
+
+    def imageToNP(self, image):
+        pass
+
+    def resizeImage(self, imgArr):
+        pass
