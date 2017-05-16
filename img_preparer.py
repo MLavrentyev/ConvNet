e956ab525feec2e76afb1d7ae1d8dac8dc64a4f0
Mark Lavrentyev <lavrema@outlook.com>
2017-05-16 11:43:29 -0400

    Finished image to np array method

--- img_preparer.py
+++ img_preparer.py
@@ -1,4 +1,4 @@
-import PIL
+from PIL import Image
 import numpy as np
 
 class ImageConverter(object):
@@ -10,8 +10,16 @@ class ImageConverter(object):
         self.imgHeight = imageSize[1]
         
 
-    def imageToNP(self, image):
-        pass
+    def imageToNPArray(self, imgPath):
+        # imgPath - string path to the rgb image
+        img = Image.open(imgPath)
 
-    def resizeImage(self, imgArr):
-        pass
+        if img.size != (self.imgWidth, self.imgHeight):
+            img.resize((self.imgWidth, self.imgHeight), Image.LANCZOS)
+
+                
+        rPix = np.array(list(img.getdata(band=0)))
+        gPix = np.array(list(img.getdata(band=1)))
+        bPix = np.array(list(img.getdata(band=2)))
+
+        return [rPix, gPix, bPix]
