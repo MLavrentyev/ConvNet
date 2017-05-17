fe30ed893c820f3542e799464a045dda80bc90cb
Mark Lavrentyev <lavrema@outlook.com>
2017-05-17 16:43:43 -0400

    Figure out the pooling layer

--- convnet.py
+++ convnet.py
@@ -16,6 +16,9 @@ class ConvNet(object):
         self.c2Filters = [np.random.random(c2FiltShape)*2-1
                           for x in range(c2NumFilts)]
 
+        self.p1Shape = p1Shape
+        self.p1Step = p1Step
+
     def forwardProp(self, imgArr):
         # imgArr - np array of size (d, w, h)
 
@@ -29,28 +32,37 @@ class ConvNet(object):
                                                          mode='constant',
                                                          cval=0.0)
         #End 1st Convolution
+        newSizes = (self.c1Out.shape[0],
+                    int((self.c1Out.shape[1]-self.p1Shape[0])/self.p1Step)+1,
+                    int((self.c1Out.shape[2]-self.p1Shape[1])/self.p1Step)+1)
 
         #Apply 1st pooling
-        self.p1Out = np.zeros((self.c1Out.shape[0],
+        self.p1Out = np.zeros(newSizes)
+        print(self.c1Out)
                                
-        for channel in self.c1Out:
-            self.p1Out[] = None                          
+        ySpots = np.array([y for y in range(0, self.c1Out.shape[1]+1-self.p1Shape[0], self.p1Step)])
+        xSpots = np.array([x for x in range(0, self.c1Out.shape[2]+1-self.p1Shape[1], self.p1Step)])
+        print(xSpots, ySpots)
+
+        for chan in range(newSizes[0]):
+            ixmesh = np.ix_([chan], np.arange(len(ySpots)), np.arange(len(xSpots)))
+            
+            self.p1Out[ixmesh] = [[np.amax(self.c1Out[chan, r:r+self.p1Shape[1], c:c+self.p1Shape[0]]) for c in xSpots] for r in ySpots]
+
+        print(self.p1Out)
 
             
 
 
 cNet = ConvNet((2,2), 1, 2,
-               (1,1), 1,
+               (2,2), 1,
                (2,2), 1, 1,
                (1,1), 1,
                15, 4)
 imgArr = np.array([[[1., 2, 3],
-                    [4, 5, 6],
-                    [7, 8, 9]],
+                    [4, 5, 6],],
                    [[0, 0, 0],
-                    [0, 0, 0],
-                    [0, 0, 0]],
+                    [0, 0, 0],],
                    [[2, 4, 6],
-                    [8, 10, 12],
-                    [14, 16, 18]]])/20
+                    [8, 10, 12]]])/20
 cNet.forwardProp(imgArr)
