de5394f9445af0134d43b8480dde1fe7e3f3d324
Mark Lavrentyev <lavrema@outlook.com>
2017-07-07 18:32:08 -0400

    Make some adjustments to simplify syntax

--- img_preparer.py
+++ img_preparer.py
@@ -5,10 +5,7 @@ class ImageConverter(object):
 
     def __init__(self, imageSize):
         # imageSize - tuple (w,h) to normalize images to
-
-        self.imgWidth = imageSize[0]
-        self.imgHeight = imageSize[1]
-        
+        self.imgWidth, self.imgHeight = imageSize
 
     def imageToNPArray(self, img):
         # img - PIL Image object
@@ -28,7 +25,7 @@ class ImageConverter(object):
         return np.array([rPix, gPix, bPix])
 
     def scaleImage(self, imgArr):
-        return imgArr/255*2 - 1
+        return imgArr/255
 
     def prepImage(self, imgPath):
         img = Image.open(imgPath)
